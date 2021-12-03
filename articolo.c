c2646d7f27cf91f4f666c8a8273842c63381f032
ilacinap <94383191+ilacinap@users.noreply.github.com>
2021-12-03 16:54:35 +0100

    Add files via upload

--- articolo.c
+++ articolo.c
@@ -14,12 +14,16 @@ FILE *fh;
     if (argc<2){printf("Non hai messo il nome dell'articolo\n");exit (-1);}
     if (argc<3){printf("Non hai messo il numero di caratteri della colonna\n");exit (-2);}
 //aprire il file dell'articolo che è in argv[1]
-    fh=fopen(argv[1], 'r');
+    fh=fopen(argv[2], 'r');
+    nc=argv[3];
+    riga=riga[nc]
 //ipotizzando che alla fine del file 'articolo' ci sia un NULL 
 //utilizzare un ciclo while che vada a leggere il file fino al null e stampi le righe per stringhe del numero di caratteri in argv[2]
     while (fgets(riga, sizeof(riga),fh)!=NULL){
         if (riga[strlen(riga)-1]=='\n') riga[strlen(riga)-1]='\0';
         printf("%s", riga);
+        nc=nc+nc;
+        riga=riga[nc]
     }
     fclose(fh);
     return (0);
